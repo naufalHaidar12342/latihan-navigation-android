5353b113375eb1301f839dbff36476d42fefb8f5
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2022-03-13 23:51:53 +0700

    masih debugging pada fragment category dan fragment detail terkait null data

--- app/src/main/java/xyz/heydarrn/latihannavigationcomponent/CategoryFragment.kt
+++ app/src/main/java/xyz/heydarrn/latihannavigationcomponent/CategoryFragment.kt
@@ -1,6 +1,7 @@
 package xyz.heydarrn.latihannavigationcomponent
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,8 +15,8 @@ class CategoryFragment : Fragment() {
     private val binding get() = _binding!!
 
     companion object {
-        val EXTRA_NAME="category name goes here"
-        val EXTRA_STOCK="stock of item X goes here"
+        val EXTRA_NAME="category_name_goes_here"
+        val EXTRA_STOCK="stock_of_item_X_goes_here"
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,15 +34,16 @@ class CategoryFragment : Fragment() {
         binding.buttonLifestyleCategory.setOnClickListener { bindCategoryButton->
             val mBundle=Bundle()
             mBundle.putString(EXTRA_NAME,"Lifestyle")
-            mBundle.putInt(EXTRA_STOCK,7)
+            mBundle.putLong(EXTRA_STOCK,7)
             bindCategoryButton.findNavController().navigate(R.id.action_categoryFragment_to_detailOfCategoryFragment)
+
+//            val sendSafeArgs=CategoryFragmentDirections.actionCategoryFragmentToDetailOfCategoryFragment()
+//            bindCategoryButton.findNavController().navigate(R.id.action_categoryFragment_to_detailOfCategoryFragment)
         }
+        binding.imgAndroid.setImageResource(R.drawable.ic_baseline_android_24)
 
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding=null
-    }
+
 
 }
--- app/src/main/java/xyz/heydarrn/latihannavigationcomponent/DetailOfCategoryFragment.kt
+++ app/src/main/java/xyz/heydarrn/latihannavigationcomponent/DetailOfCategoryFragment.kt
@@ -1,6 +1,7 @@
 package xyz.heydarrn.latihannavigationcomponent
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -29,12 +30,24 @@ class DetailOfCategoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val name=arguments?.getString(CategoryFragment.EXTRA_NAME)
-        val description=arguments?.getInt(CategoryFragment.EXTRA_STOCK)
+        val description=arguments?.getLong(CategoryFragment.EXTRA_STOCK)
 
         binding.textViewCategoryDescription.text="Stock : $description"
+        Log.d("stok kiriman", binding.textViewCategoryDescription.text.toString())
         binding.textViewCategoryName.text=name
+        Log.d("nama kiriman", binding.textViewCategoryName.text.toString())
+//        val dataName=DetailOfCategoryFragmentArgs.fromBundle(arguments as Bundle).namaProduk
+//        val dataItemStock=DetailOfCategoryFragmentArgs.fromBundle(arguments as Bundle).stokProduk
+//
+//        binding.textViewCategoryName.text=dataName
+//        Log.d("nama kiriman", binding.textViewCategoryName.text.toString())
+//        binding.textViewCategoryDescription.text="Stok : $dataItemStock"
+//        Log.d("stok kiriman", binding.textViewCategoryDescription.text.toString())
     }
-
+    companion object {
+        val EXTRA_NAME="category name goes here"
+        val EXTRA_STOCK="stock of item X goes here"
+    }
     override fun onDestroy() {
         super.onDestroy()
         _binding=null
